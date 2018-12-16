cefc7ca11829e7bb0000f409c67cb1563d416850
Edgardo Ramirez <edgardo.d.ramirez@gmail.com>
2018-12-16 18:05:48 -0300

    Version validada

--- appIMDB/AppIMDB.DataBase/dbo/Tables/Actor.sql
+++ appIMDB/AppIMDB.DataBase/dbo/Tables/Actor.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL IDENTITY PRIMARY KEY, 
     [Name] VARCHAR(50) NOT NULL, 
     [BirthDate] DATETIME NOT NULL, 
-    [Nationality] VARCHAR(50) NULL
+    [Nationality] VARCHAR(50) NULL, 
+    [Image] IMAGE NULL
 )
